4d43d0dd377e9f109b4d1fea46788ab4da70b8aa
JoelChiaraviglio <chiaravigliobrianjoel@gmail.com>
2022-03-04 21:11:32 -0300

    First commit

---
+++ HelloWorld.cs
@@ -0,0 +1,13 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+//Modelando un objeto
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Hello World");
+    }
+}
